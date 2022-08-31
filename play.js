f961c12dcc406bfdec995619f2d5e7b8315c4299
originallykevin <ly1@ualberta.ca>
2022-08-31 20:42:14 +0000

    initial commit.

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "10.0.2.15",
+    port: "50541",
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("event name", () => {
+    // code that does something
+  });
+
+  conn.on("connect", (data) => {
+    console.log(`Welcome to Snek`)
+  });
+
+  return conn;
+};
+
+
+console.log("Connecting ...");
+connect();
