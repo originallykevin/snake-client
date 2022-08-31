f3fbd3c7f6dd5c9997e59c7baf19526c1ecd5fdd
originallykevin <ly1@ualberta.ca>
2022-08-31 21:07:59 +0000

    create disconnection message.

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "10.0.2.15",
     port: "50541",
@@ -10,11 +10,20 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("data", (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  conn.on("end", () => {
+    console.log("You have died");
+  });
+
   conn.on("connect", () => {
-    console.log(`Welcome to Snek`)
+    console.log(`Welcome to Snek`);
   });
 
   return conn;
 };
 
-module.exports = connect
+module.exports = connect;
