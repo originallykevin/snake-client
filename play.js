f1169dadcddb0783b55b89087d6f424b4bc2977b
originallykevin <ly1@ualberta.ca>
2022-08-31 20:54:57 +0000

    refactor with module.export to client.js.

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const net = require("net");
+const connect = require('./client')
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: "50541",
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("event name", () => {
-    // code that does something
-  });
-
-  conn.on("connect", (data) => {
-    console.log(`Welcome to Snek`)
-  });
-
-  return conn;
-};
 
 
 console.log("Connecting ...");
 connect();
+
